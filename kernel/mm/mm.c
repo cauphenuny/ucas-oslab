41a4aa5e70710960b4d6bcb50fad8dd5996d0105
cauphenuny <starseekist@gmail.com>
2025-12-02 21:56:37 +0800

    p4-task1: impl add_page, share_pgtable, alloc_page_helper

--- kernel/mm/mm.c
+++ kernel/mm/mm.c
@@ -1,4 +1,5 @@
 #include <os/mm.h>
+#include <logger.h>
 
 // NOTE: A/C-core
 static ptr_t kernMemCurr = FREEMEM_KERNEL;
@@ -33,11 +34,27 @@ void *kmalloc(size_t size)
     // TODO [P4-task1] (design you 'kmalloc' here if you need):
 }
 
+static inline uintptr_t add_page(uint64_t vpn, PTE* pgdir)
+{
+    ptr_t new_page = allocPage(1);
+    set_pfn(&pgdir[vpn], kva2pa(new_page) >> NORMAL_PAGE_SHIFT);
+    set_attribute(&pgdir[vpn], _PAGE_PRESENT);
+    return new_page;
+}
 
 /* this is used for mapping kernel virtual address into user page table */
 void share_pgtable(uintptr_t dest_pgdir, uintptr_t src_pgdir)
 {
-    // TODO [P4-task1] share_pgtable:
+    // DONE [P4-task1] share_pgtable:
+    PTE* src = (PTE*)src_pgdir;
+    PTE* dest = (PTE*)dest_pgdir;
+    for (int i = 0; i < PTE_ENTRY_NUM; i++) {
+        PTE src_entry = src[i];
+        if (get_attribute(src_entry, _PAGE_PRESENT)) {
+            asserts(!get_attribute(dest[i], _PAGE_PRESENT), "share_pgtable: dest entry already present");
+            dest[i] = src_entry;
+        }
+    }
 }
 
 /* allocate physical page for `va`, mapping it into `pgdir`,
@@ -45,7 +62,20 @@ void share_pgtable(uintptr_t dest_pgdir, uintptr_t src_pgdir)
    */
 uintptr_t alloc_page_helper(uintptr_t va, uintptr_t pgdir)
 {
-    // TODO [P4-task1] alloc_page_helper:
+    // DONE [P4-task1] alloc_page_helper
+    va &= VA_MASK;
+    uint64_t vpn2 = va >> (NORMAL_PAGE_SHIFT + PPN_BITS + PPN_BITS);
+    uint64_t vpn1 = (vpn2 << PPN_BITS) ^ (va >> (NORMAL_PAGE_SHIFT + PPN_BITS));
+    uint64_t vpn0 = ((vpn2 << (PPN_BITS + PPN_BITS)) + (vpn1 << PPN_BITS)) ^ (va >> NORMAL_PAGE_SHIFT);
+    PTE* current_pgdir = (PTE*)pgdir;
+    if (current_pgdir[vpn2] == 0) clear_pgdir(add_page(vpn2, current_pgdir));
+    current_pgdir = (PTE*)pa2kva(get_pa(current_pgdir[vpn2]));
+    if (current_pgdir[vpn1] == 0) clear_pgdir(add_page(vpn1, current_pgdir));
+    current_pgdir = (PTE*)pa2kva(get_pa(current_pgdir[vpn1]));
+    asserts(current_pgdir[vpn0] == 0, "alloc_page_helper: page already allocated");
+    uintptr_t new_page = add_page(vpn0, current_pgdir);
+    pretty_log(LOG_INFO, "va 0x%lx mapped to new page 0x%lx", va, new_page);
+    return new_page;
 }
 
 uintptr_t shm_page_get(int key)

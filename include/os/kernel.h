7e5267cff8e65bda91130d1dd72df487a81617cc
cauphenuny <starseekist@gmail.com>
2025-12-05 11:56:41 +0800

    p4-task3: fix Makefile: alloc 64MiB instead of MB; detect bios_sd_* error

--- include/os/kernel.h
+++ include/os/kernel.h
@@ -1,6 +1,7 @@
 #ifndef __INCLUDE_KERNEL_H__
 #define __INCLUDE_KERNEL_H__
 
+#include <logger.h>
 #include <type.h>
 #include <common.h>
 #include <pgtable.h>
@@ -39,13 +40,21 @@ static inline void bios_putchar(int ch) { call_jmptab(CONSOLE_PUTCHAR, (long)ch,
 static inline int bios_getchar(void) { return call_jmptab(CONSOLE_GETCHAR, 0, 0, 0, 0, 0); }
 
 static inline int bios_sd_read(uint64_t mem_address, unsigned num_of_blocks, unsigned block_id) {
-    return call_jmptab(SD_READ, (long)kva2pa(mem_address), (long)num_of_blocks, (long)block_id, 0, 0);
+    int ret = call_jmptab(SD_READ, (long)kva2pa(mem_address), (long)num_of_blocks, (long)block_id, 0, 0);
+    if (ret) {
+        pretty_loge("bios sd read error: mem_address=%lu, num_of_blocks=%u, block_id=%u", mem_address, num_of_blocks, block_id);
+    }
+    return ret;
 }
 
 /************************************************************/
 
 static inline int bios_sd_write(uint64_t mem_address, unsigned num_of_blocks, unsigned block_id) {
-    return call_jmptab(SD_WRITE, (long)kva2pa(mem_address), (long)num_of_blocks, (long)block_id, 0, 0);
+    int ret = call_jmptab(SD_WRITE, (long)kva2pa(mem_address), (long)num_of_blocks, (long)block_id, 0, 0);
+    if (ret) {
+        pretty_loge("bios sd write error: mem_address=%lu, num_of_blocks=%u, block_id=%u", mem_address, num_of_blocks, block_id);
+    }
+    return ret;
 }
 
 static inline void bios_logging(char* str) { call_jmptab(QEMU_LOGGING, (long)str, 0, 0, 0, 0); }

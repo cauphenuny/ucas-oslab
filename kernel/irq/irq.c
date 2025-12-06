e8705419760d2854fe134c9577a45547b9ef55bc
cauphenuny <starseekist@gmail.com>
2025-12-07 02:24:23 +0800

    p4-task5: enable on_access/write handling in handle_page_fault

--- kernel/irq/irq.c
+++ kernel/irq/irq.c
@@ -164,15 +164,15 @@ void handle_page_fault(regs_context_t* regs, uint64_t stval, uint64_t scause) {
         alloc_page(stval, current_running->pgdir, false);
         return;
     }
-    // if (get_attribute(*pte, _PAGE_PRESENT)) {
-    //     pageframe_group_t* group = get_current_pagegroup();
-    //     if (scause == EXCC_LOAD_PAGE_FAULT || scause == EXCC_INST_PAGE_FAULT) {
-    //         group->vtable->on_access(group, stval, get_ticks());
-    //     } else {
-    //         group->vtable->on_write(group, stval, get_ticks());
-    //     }
-    //     return;
-    // }
+    if (get_attribute(*pte, _PAGE_PRESENT)) {
+        pageframe_group_t* group = get_current_pagegroup();
+        if (scause == EXCC_LOAD_PAGE_FAULT || scause == EXCC_INST_PAGE_FAULT) {
+            group->vtable->on_access(group, stval, get_ticks());
+        } else {
+            group->vtable->on_write(group, stval, get_ticks());
+        }
+        return;
+    }
     if (get_attribute(*pte, _PAGE_SOFT)) {
         kva_t new_page = alloc_pageframe(get_current_pagegroup(), 1);
         swapin(stval, current_running->pgdir, new_page);

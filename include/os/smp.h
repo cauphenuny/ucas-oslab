fa51c0672a4801d911e4b7051667e7e72df7821b
cauphenuny <starseekist@gmail.com>
2025-12-07 00:39:35 +0800

    p4-task1: when kernel exception occurs, halt

--- include/os/smp.h
+++ include/os/smp.h
@@ -7,7 +7,8 @@
 extern void init_smp();
 extern void wakeup_other_hart();
 extern uint64_t get_current_cpu_id();
-extern void lock_kernel();
+struct regs_context;
+extern void lock_kernel(struct regs_context* regs, uint64_t stval, uint64_t scause, uint64_t sepc);
 extern void unlock_kernel();
 
 #endif /* SMP_H */
--- kernel/smp/smp.c
+++ kernel/smp/smp.c
@@ -1,38 +1,39 @@
-#include <logger.h>
 #include <atomic.h>
+#include <logger.h>
+#include <os/irq.h>
+#include <os/kernel.h>
+#include <os/lock.h>
 #include <os/sched.h>
 #include <os/smp.h>
-#include <os/lock.h>
-#include <os/kernel.h>
+#include <type.h>
 
 static spin_lock_t kernel_lock;
 
-void init_smp()
-{
+void init_smp() {
     /* DONE: P3-TASK3 multicore*/
     spin_lock_init(&kernel_lock);
 }
 
-void wakeup_other_hart()
-{
+void wakeup_other_hart() {
     unsigned long mask = (1 << NR_CPUS) - 1;
     int hartid = get_current_cpu_id();
-    mask = ~((~mask) | (1 << hartid)); // do not send ipi to self
+    mask = ~((~mask) | (1 << hartid));  // do not send ipi to self
     pretty_log(LOG_WARN, "send ipi with mask 0x%x", mask);
     send_ipi(&mask);
 }
 
-void lock_kernel()
-{
-    // static int tot = 0;
-    // int id = tot++;
-    // pretty_log(LOG_INFO, "locking kernel... (#%d, cur=%d)", id, kernel_lock.status);
+void lock_kernel(regs_context_t* regs, uint64_t stval, uint64_t scause, uint64_t sepc) {
+    if ((sepc & (1ul << 63)) && scause != (IRQC_S_TIMER | SCAUSE_IRQ_FLAG)) {
+        // NOTE: exception occured in kernel code
+        pretty_loge(
+            "exception in kernel mode! sepc=0x%lx, scause=%lu, stval=0x%lx", sepc, scause, stval);
+        handle_other(regs, stval, scause);
+        return;
+    }
     spin_lock_acquire(&kernel_lock);
-    // pretty_log(LOG_INFO, "locked (#%d)", id);
 }
 
-void unlock_kernel()
-{
+void unlock_kernel() {
     // pretty_log(LOG_INFO, "kernel unlocked.");
     spin_lock_release(&kernel_lock);
 }
